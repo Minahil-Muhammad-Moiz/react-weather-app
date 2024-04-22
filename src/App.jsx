d90383912d66fa08203ad1b22551fb2c34fb71f1
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-23 03:13:49 +0500

    fetched weather report of the city, provided in input field + useQuery Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import Axios  from "axios";
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+
 function App() {
+  const [place, setPlace ] = useState('')
+
+  const {data, refetch}= useQuery({
+    queryKey:['weather'],
+    queryFn:async()=>{
+      return await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=dd94f859a0e52d6e4767fddf735f04a7`).then((res)=>res.data.main)
+    }
+  })
+  console.log(data);
+
+  const handleChange=()=>{
+    refetch(place)
+  }
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className=" w-[600px] h-[500px] backdrop-blur-md bg-white/20 absolute top-0 left-0 right-0 bottom-0 m-auto rounded-lg">
+        <input value={place} onChange={(e)=>setPlace(e.target.value)} />
+        <BsSearch onClick={handleChange}/>
+      </div>
+
     </>
   );
 }
