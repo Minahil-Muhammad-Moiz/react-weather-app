dd034feed5dcd5227cf629bda1b577a0175aaa3d
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-24 13:13:46 +0500

    Fixed CSS and updated props.place to uppercase

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,16 @@ function App() {
   };
   return (
     <>
-      <div className={classNames(parentDiv)}>
-        <div className="relative">
+      <div className={classNames("p-5 w-[600px] h-[500px] backdrop-blur-3xl absolute top-0 left-0 right-0 bottom-0 m-auto rounded-lg")}>
+        <div className="relative ">
           <input
             value={inputVal}
             onChange={(e) => setInputVal(e.target.value)}
-            className={classNames("w-full p-5 rounded-lg outline-none")}
+            className={classNames("w-full p-5 rounded-3xl outline-none")}
             placeholder="Enter City Name..."
           />
-          <HiSearch onClick={handleChange} className={classNames(searchIcon)} />
-          <WeatherDisplay place={place}/>
+          <HiSearch onClick={handleChange} className={classNames(' hover:text-orange-400 rounded-lg ',searchIcon)} />
+          {place && <WeatherDisplay place={place.toUpperCase()}/>}
         </div>
       </div>
     </>
@@ -30,7 +30,6 @@ function App() {
 
 export default App;
 
-const parentDiv =
-  "p-5 w-[600px] h-[500px] backdrop-blur-md bg-white/20 absolute top-0 left-0 right-0 bottom-0 m-auto rounded-lg";
+// const parentDiv =;
 const searchIcon =
-  "absolute right-4 h-8 w-8 top-4 transition-all hover:scale-50";
+  "absolute right-4 h-8 w-8 top-4 transition-all ";
